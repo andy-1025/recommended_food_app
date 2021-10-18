57e21b4de933ec8f2fa57f6c650f302480f12f97
安藤誠二 <hands1025@gmail.com>
2021-10-19 01:33:21 +0900

    外部キーとバリデーション修正

--- db/migrate/20211018160308_create_foods.rb
+++ db/migrate/20211018160308_create_foods.rb
@@ -1,8 +1,9 @@
 class CreateFoods < ActiveRecord::Migration[6.1]
   def change
     create_table :foods do |t|
-      t.string :name
+      t.string :name, null: false
       t.text :comment
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
