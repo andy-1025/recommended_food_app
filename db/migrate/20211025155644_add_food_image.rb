9f14c6b73a2cc92612f41fa12109fd8b99249b8f
安藤誠二 <hands1025@gmail.com>
2021-10-27 01:14:15 +0900

    不要なマイグレファイル削除

--- db/migrate/20211025155644_add_food_image.rb
+++
@@ -1,5 +0,0 @@
-class AddFoodImage < ActiveRecord::Migration[6.1]
-  def change
-    add_column :foods, :image, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_25_155644) do
+ActiveRecord::Schema.define(version: 2021_10_25_132659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "food_images", force: :cascade do |t|
-    t.string "title"
-    t.string "image"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "foods", force: :cascade do |t|
     t.string "name", null: false
