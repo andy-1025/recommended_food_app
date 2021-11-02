dcb0791ef48267bf828c8d2cae8301465c76b265
安藤誠二 <hands1025@gmail.com>
2021-11-03 01:47:39 +0900

    いいねボタンエラー解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "foods#index"
-  resources :foods
+  devise_for :users
+  resources :foods do
+    resource :likes, only: [:create, :destroy]
+  end
 end
