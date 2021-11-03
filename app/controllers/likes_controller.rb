ed7d44ae8e4a795f24f8b82e2e9a2915ce4f04d5
安藤誠二 <hands1025@gmail.com>
2021-11-03 16:01:11 +0900

    rubocop修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,14 +2,10 @@ class LikesController < ApplicationController
   def create
     @food = Food.find(params[:food_id])
     current_user.likes.create!(food_id: @food.id)
-    # current_user.likes.create!(food_id: params[:food_id])
-    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @food = Food.find(params[:food_id])
     current_user.likes.find_by(food_id: @food.id).destroy!
-    # current_user.likes.find_by(food_id: params[:food_id]).destroy!
-    # redirect_back(fallback_location: root_path)
   end
 end
